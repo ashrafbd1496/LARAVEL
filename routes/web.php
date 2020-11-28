7520ad1335168869ddb7b5c1aabcf5c89a6acaf2
ashrafuddin1496 <asraf1496@gmail.com>
2020-11-28 21:56:12 +0600

    index and table added into route

--- routes/web.php
+++ routes/web.php
@@ -14,59 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::get('/', function(){
+	return view('index');
+});
 
-
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('table', function(){
+	return view('table');
 });
 
 
-//Resource controller
-Route::resource('teacher','TeacherController');
-
-//student route
-
-Route::get('student', 'StudentController@index');
-
-
-
-//dev route - 
-Route::get('dev',function(){
-	echo "Developoer";
-});
-
-
-
-
-
-//Name route
-Route::get('samim',function(){
-	echo "My name is shamim";
-})->name('shamim-ahmed');
-
-Route::get('sakir',function(){
-	echo "My name is sakir";
-})->name('sakir-ahmed');
-
-
-//prefix route
-
-Route::prefix('user')->group(function () {
-
-Route::get('about-us',function(){
-	echo "It's about us page";
-});
-
-Route::get('contact-us',function(){
-
-	echo "It's Contact us page";
-
-});
-
-Route::get('dev/{name?}/{age?}',function($name='Ashraf', $age='30'){
-	echo "My name is $name and age is $age";
-});
-
-
-});//end of the prefix route
